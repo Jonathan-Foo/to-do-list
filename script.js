27753d7b3896ce32d47bcaf10e9f1cd2f008c1ed
Jonathan Foo <Johnathan.foo@gmail.com>
2022-03-26 21:19:11 +1100

    Minor Changes To Element Classes and IDs

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-localStorage.setItem('todos', '{"home": [{"name": "bob"}, {"name": "steve"}], "today": ["item", "item2"], "week": ["item", "item2"]}')
+// localStorage.setItem('todos', '{"home": [{"name": "bob"}, {"name": "steve"}], "today": ["item", "item2"], "week": ["item", "item2"]}')
 
-let todos = JSON.parse(localStorage.todos)
+// let todos = JSON.parse(localStorage.todos)
 
-todos.home.splice(0, 1)
+// todos.home.splice(0, 1)
 
-localStorage.setItem('todos', JSON.stringify(todos))
-console.log(todos)
+// localStorage.setItem('todos', JSON.stringify(todos))
+// console.log(todos)
 
 
-let a = [1, 2, 3, 4, 5, 6]
-let index = 3
-let b = a.slice(0, index).concat(a.slice(index + 1)) 
-console.log(b) 
+// let a = [1, 2, 3, 4, 5, 6]
+// let index = 3
+// let b = a.slice(0, index).concat(a.slice(index + 1)) 
+// console.log(b) 
+
+
+const tabsParent = [...document.querySelectorAll('.tab-name')].map(project => project.parentElement)
+// const tabs = [...document.querySelectorAll('.tab-name')]
+
+tabsParent.forEach(tab => tab.addEventListener('click', (e) => {
+    console.log(e.currentTarget.querySelector('.tab-name').textContent)
+}))
+
+// tabs.forEach(tab => tab.addEventListener('click', (e) => {
+//     console.log(e.currentTarget)
+// }))
+
+
+
+// console.log(projects.length > 0)
